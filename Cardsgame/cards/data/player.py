43a385d37e66e6c4a6e3b73f33f4584c2c1b4aa8
kosorio2 <oso16001@byui.edu>
2021-03-19 14:16:03 -0600

    further updates

--- Cardsgame/cards/data/player.py
+++ Cardsgame/cards/data/player.py
@@ -83,24 +83,24 @@ def get_enemy_selection(health):
         return random.randint(1, 3)
 
 
-def play_round(enemy, human):
+def play_round(enemy, avatar):
     game_in_progress = True
     current_player = enemy
 
     while game_in_progress:
         # swap the current player each round
         if (current_player == enemy):
-            current_player = human
+            current_player = avatar
         else:
             current_player = enemy
 
         print()
         print(
-            f"You have {human.health} health remaining and the "
+            f"You have {avatar.health} health remaining and the "
             f"Enemy has {enemy.health} health remaining.")
         print()
 
-        if (current_player == human):
+        if (current_player == avatar):
             print("Available attacks:")
             print("1) Electrocute - Causes moderate damage.")
             print("2) Wild Swing - high or low damage, "
@@ -114,69 +114,69 @@ def play_round(enemy, human):
 
         if (move == 1):
             damage = random.randrange(18, 25)
-            if (current_player == human):
-                enemy.calculate_damage(damage, human.name.capitalize())
+            if (current_player == avatar):
+                enemy.calculate_damage(damage, avatar.name.capitalize())
             else:
-                human.calculate_damage(damage, enemy.name.capitalize())
+                avatar.calculate_damage(damage, enemy.name.capitalize())
         elif (move == 2):
             damage = random.randrange(10, 35)
-            if (current_player == human):
-                enemy.calculate_damage(damage, human.name.capitalize())
+            if (current_player == avatar):
+                enemy.calculate_damage(damage, avatar.name.capitalize())
             else:
-                human.calculate_damage(damage, enemy.name.capitalize())
+                avatar.calculate_damage(damage, enemy.name.capitalize())
         elif (move == 3):
             heal = random.randrange(18, 25)
             current_player.calculate_heal(heal)
         elif (move == 4):
             damage = random.choice([0, 20])
-            if (current_player == human):
-                enemy.calculate_damage(damage, human.name.capitalize())
+            if (current_player == avatar):
+                enemy.calculate_damage(damage, avatar.name.capitalize())
             else:
-                human.calculate_damage(damage, enemy.name.capitalize())
+                avatar.calculate_damage(damage, enemy.name.capitalize())
         elif (move == 5):
             damage = random.choice([5, 15])
             if damage == 15:
-                if (current_player == human):
-                    enemy.calculate_damage(damage, human.name.capitalize())
+                if (current_player == avatar):
+                    enemy.calculate_damage(damage, avatar.name.capitalize())
                 else:
-                    human.calculate_damage(damage, enemy.name.capitalize())
+                    avatar.calculate_damage(damage, enemy.name.capitalize())
             else:
-                if (current_player == human):
-                    human.calculate_damage(damage, human.name.capitalize())
+                if (current_player == avatar):
+                    avatar.calculate_damage(damage, avatar.name.capitalize())
                 else:
                     enemy.calculate_damage(damage, enemy.name.capitalize())
         else:
             print ("The input was not valid. Please select a choice again.")
 
-        if (human.health == 0):
+        if (avatar.health == 0):
             delay_print("Sorry, you lose!")
             enemy.wins += 1
             game_in_progress = False
 
         if (enemy.health == 0):
             print("Congratulations, you beat the enemy!")
-            human.wins += 1
+            avatar.wins += 1
             game_in_progress = False
 
 
 def start_game():
-    print("Welcome to the As-Yet-Unnamed turn-based battle game!")
+    print("Welcome to the battle!")
 
     enemy = Player("Enemy")
 
     name = input("Please enter your name: ")
-    human = Player(name)
+    avatar = Player(name)
 
     keep_playing = True
 
     while (keep_playing is True):
         print("Current Score:")
-        print(f"You - {human.wins}")
+        print(f"You - {avatar.wins}")
         print(f"Enemy - {enemy.wins}")
 
         enemy.health = 100
-        human.health = 100
-        play_round(enemy, human)
+        avatar.health = 100
+        play_round(enemy, avatar)
         print()
         response = input("Play another round?(Y/N)")
         if (response.lower() == "n"):
