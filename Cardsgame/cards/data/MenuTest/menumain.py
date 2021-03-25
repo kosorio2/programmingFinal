bfe874cc1c1c80ef48b48bc58364bec89ffd9cd3
kosorio2 <oso16001@byui.edu>
2021-03-25 16:26:29 -0600

    added a few sound options

--- Cardsgame/cards/data/MenuTest/menumain.py
+++ Cardsgame/cards/data/MenuTest/menumain.py
@@ -1,7 +1,18 @@
 from game import Game
+from background_sound import MySound
+import arcade
 
 g = Game()
 
 while g.running:
     g.curr_menu.display_menu()
     g.game_loop()
+    
+    def main():
+        """ Main method """
+        window = MySound()
+        window.setup()
+        arcade.run()
+
+    if __name__ == "__main__":
+        main()
